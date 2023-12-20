468e4de8501e96808d78617159e206a0151cd7b5
knhn1004 <49494541+knhn1004@users.noreply.github.com>
2023-12-19 18:37:50 -0800

    Create Restaurant Form

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Restaurant;
+use App\Models\City;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Enums\RoleName;
+use App\Http\Requests\Admin\StoreRestaurantRequest;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
+
+use App\Notifications\RestaurantOwnerInvitation;
 
 class RestaurantController extends Controller
 {
@@ -15,6 +24,41 @@ class RestaurantController extends Controller
 
         return Inertia::render('Admin/Restaurants/Index', [
             'restaurants' => Restaurant::with(['city', 'owner'])->get(),
+            'cities' => City::get(['id', 'name']), // Use the City model class
         ]);
+    }
+
+    public function create()
+    {
+        $this->authorize('restaurant.create');
+
+        return Inertia::render('Admin/Restaurants/Create', [
+            'cities' => City::get(['id', 'name']),
+        ]);
+    }
+
+    public function store(StoreRestaurantRequest $request): RedirectResponse
+    {
+        $validated = $request->validated();
+
+        DB::transaction(function () use ($validated) {
+            $user = User::create([
+                'name'     => $validated['owner_name'],
+                'email'    => $validated['email'],
+                'password' => '',
+            ]);
+
+            $user->roles()->sync(Role::where('name', RoleName::VENDOR->value)->first());
+
+            $user->restaurant()->create([
+                'city_id' => $validated['city_id'],
+                'name'    => $validated['restaurant_name'],
+                'address' => $validated['address'],
+            ]);
+
+            $user->notify(new RestaurantOwnerInvitation($validated['restaurant_name']));
+        });
+
+        return to_route('admin.restaurants.index');
     }
 }
